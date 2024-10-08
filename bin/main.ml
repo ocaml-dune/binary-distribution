4d35de7b385e46ce8a33d50fa900eb01e696cb74
Etienne Marais <dev@maiste.fr>
2024-10-08 13:57:25 +0200

    feat: remove gen-html function
    Signed-off-by: Etienne Marais <dev@maiste.fr>

--- bin/main.ml
+++ bin/main.ml
@@ -2,11 +2,6 @@ open Sandworm
 open Cmdliner
 
 module Common_args = struct
-  let html_file =
-    let doc = "The file where to export the HTML code." in
-    Arg.(value & opt string Config.Path.html_index & info ~doc [ "html" ])
-  ;;
-
   let metadata_file =
     let doc = "The JSON file to import the data from." in
     Arg.(value & opt string Config.Path.metadata & info ~doc [ "metadata" ])
@@ -33,31 +28,8 @@ module Common_args = struct
   ;;
 end
 
-module Gen_html = struct
-  let generate_website html_file metadata_file =
-    Format.printf "--> Generate the index %s\n" html_file;
-    let bundles = Metadata.import_from_json metadata_file in
-    Web.export_bundle_to_file ~base_url:Config.Server.url ~file:html_file bundles;
-    Format.printf "--> Completed ✓\n"
-  ;;
-
-  let term =
-    let open Term.Syntax in
-    let+ html_file = Common_args.html_file
-    and+ metadata_file = Common_args.metadata_file in
-    generate_website html_file metadata_file
-  ;;
-
-  let info =
-    let doc = "Generate the website HTML code from metadata to a file." in
-    Cmd.info "gen-html" ~doc
-  ;;
-
-  let cmd = Cmd.v info term
-end
-
 module Sync = struct
-  let synchronise html_file metadata_file commit dry_run =
+  let synchronise metadata_file commit dry_run =
     Format.printf "--> Start synchronisation\n";
     let bundle = Metadata.import_from_json metadata_file in
     let daily_bundle = Metadata.(Bundle.create_daily ~commit Target.defaults) in
@@ -102,23 +74,17 @@ module Sync = struct
       then Format.printf "- Export metadata to %s\n" metadata_file
       else Metadata.export_to_json metadata_file bundles
     in
-    let () =
-      if dry_run
-      then Format.printf "- Export HTML code to %s\n" html_file
-      else Web.export_bundle_to_file ~base_url:Config.Server.url ~file:html_file bundles
-    in
     Format.printf "--> Completed ✓\n"
   ;;
 
   let term =
     let open Term.Syntax in
-    let+ html_file = Common_args.html_file
-    and+ metadata_file = Common_args.metadata_file
+    let+ metadata_file = Common_args.metadata_file
     and+ commit = Common_args.commit
     and+ dry_run = Common_args.dry_run in
     match commit with
     | None -> failwith "Commit is mandatory"
-    | Some commit -> synchronise html_file metadata_file commit dry_run
+    | Some commit -> synchronise metadata_file commit dry_run
   ;;
 
   let info =
@@ -156,6 +122,6 @@ let info = Cmd.info "sandworm"
 let root_term = Term.ret (Term.const (`Help (`Pager, None)))
 
 let () =
-  let cmd = Cmd.group ~default:root_term info [ Sync.cmd; Gen_html.cmd; Http.cmd ] in
+  let cmd = Cmd.group ~default:root_term info [ Sync.cmd; Http.cmd ] in
   Cmd.eval cmd |> exit
 ;;
