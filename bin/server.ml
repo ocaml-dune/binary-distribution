c369f37a84214e82560f11e2ce9951881e30edfd
Etienne Marais <dev@maiste.fr>
2024-10-15 06:53:17 +0000

    feat: return latest url (#84)
    Signed-off-by: Etienne Marais <dev@maiste.fr>

--- bin/server.ml
+++ bin/server.ml
@@ -13,7 +13,17 @@ let reload_script_middleware ~dev inner_handler request =
   if dev then Dream.livereload inner_handler request else inner_handler request
 ;;
 
-let serve ~dev site port =
+let latest_route_from_targets ~base_url bundle =
+  let open Sandworm.Metadata in
+  List.map
+    (fun target ->
+      let path = Format.sprintf "/%s" (Target.to_string target) in
+      Dream.get path (fun _ ->
+        Dream.html (Bundle.to_download_url ~base_url ~target bundle)))
+    bundle.targets
+;;
+
+let serve ~dev ~base_url site port bundle =
   let interface = if dev then "127.0.0.1" else "0.0.0.0" in
   Dream.log
     "Server is up and running with mode %s and with css %s"
@@ -30,5 +40,6 @@ let serve ~dev site port =
        ; Dream.get "/index.html" (fun _ -> Dream.html site)
        ; Dream.get "/install" (fun request -> Dream.redirect request "/static/install")
        ; Dream.get "/static/**" @@ Dream.static "static"
+       ; Dream.scope "/latest" [] (latest_route_from_targets ~base_url bundle)
        ]
 ;;
