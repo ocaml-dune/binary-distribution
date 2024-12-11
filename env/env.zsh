dadb85415de5ecd9fb3ce01f4b5d8f7f109ac924
Marek Kubica <marek@tarides.com>
2024-12-11 16:03:12 +0100

    Install to `.local` instead of `.dune` (#143)
    Signed-off-by: Marek Kubica <marek@tarides.com>

--- env/env.zsh
+++ env/env.zsh
@@ -1,6 +1,18 @@
 #!/usr/bin/env zsh
-export PATH="$HOME/.dune/bin:$PATH"
+
+# add path if not already done, inspired by rustup
+# affix colons on either side of $PATH to simplify matching
+case ":${PATH}:" in
+    *:"$HOME/.local/bin":*)
+        ;;
+    *)
+        # Prepending path in case a system-installed dune needs to be overridden
+        export PATH="$HOME/.local/bin:$PATH"
+        ;;
+esac
+
+# completions via bash compat
 autoload -Uz compinit bashcompinit
 compinit
 bashcompinit
-source "$HOME/.dune/completions/bash.sh"
+source "$HOME/.local/share/dune/completions/bash.sh"
